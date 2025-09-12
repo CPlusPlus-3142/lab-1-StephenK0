9d39d8d17bf5c12306243187e3d8baf57e5b2476
Stephen <159204715+StephenK0@users.noreply.github.com>
2025-09-12 14:52:47 -0400

    Add required methods to app.cpp

--- src/app.cpp
+++ src/app.cpp
@@ -16,3 +16,46 @@ int main()
     return 0;          
 }
 ****/
+/*int main()
+{
+    cout << closestPrime(8) << "\n";
+    cout << closestPrime(27) << "\n";
+    cout << closestPrime(90) << "\n";
+    cout << closestPrime(2) << "\n";
+    cout << closestPrime(1) << "\n";
+    cout << closestPrime(3) << "\n";
+    cout << closestPrime(7) << "\n";
+    cout << closestPrime(79) << "\n";
+    cout << closestPrime(67) << "\n";
+    cout << closestPrime(5) << "\n";
+    return 0;          
+}
+*/
+
+
+//Checks whether an int is prime. 
+//Currently runs in O(x) time. 
+//
+//Note that by saving past values, time complexity for repeated calls could be reduced. 
+//Note also that 1, 0, and negative numbers are not considered prime nor composite. If any of these are entered as a parameter, the function will return false. 
+bool isPrime(int x){
+    if(x <= 1) return false;
+    for(int i = 2; i <= (x / 2); i++){
+        if((x % i) == 0) return false;
+        if((x) == 0) return false;
+    }
+    return true;
+}
+
+//Returns the closest prime to x that is larger than x.
+
+int closestPrime(int x){
+    int counter = 0;
+    
+    while(true){
+        //cout << x + counter; //For Debugging Purposes. 
+        if(isPrime((x + counter))) return (x + counter);
+        counter++;
+    }
+    return -1;
+}
